ec0b8ebe183efff6fb7dbd722d244b7b6837bf71
Shaan Savarirayan <shaan.savarirayan@gmail.com>
2023-04-19 13:57:28 -0400

    Add RelayerClient methods

--- oz_defender/relay/client.py
+++ oz_defender/relay/client.py
@@ -1,3 +1,6 @@
+import datetime
+import typing
+
 import requests
 from pycognito.utils import RequestsSrpAuth
 
@@ -6,7 +9,7 @@ from .exceptions import RelayException, RelayTimeoutError
 
 class BaseClient:
     @property
-    def headers(self):
+    def headers(self) -> dict:
         return {
             "Accept": "application/json",
             "Content-Type": "application/json",
@@ -14,7 +17,7 @@ class BaseClient:
         }
 
     @property
-    def auth(self):
+    def auth(self) -> RequestsSrpAuth:
         return RequestsSrpAuth(
             username=self.api_key,
             password=self.api_secret,
@@ -23,7 +26,7 @@ class BaseClient:
             user_pool_region=self.aws_srp_pool_region,
         )
 
-    def get(self, path, params=None):
+    def get(self, path: str, params: dict = None) -> requests.Response:
         try:
             response = requests.get(
                 self.base_api + path,
@@ -37,7 +40,7 @@ class BaseClient:
 
         return response
 
-    def post(self, path, payload=None):
+    def post(self, path: str, payload: dict = None) -> requests.Response:
         try:
             response = requests.post(
                 self.base_api + path,
@@ -51,7 +54,7 @@ class BaseClient:
 
         return response
 
-    def put(self, path, payload=None):
+    def put(self, path: str, payload: dict = None) -> requests.Response:
         try:
             response = requests.put(
                 self.base_api + path,
@@ -65,7 +68,7 @@ class BaseClient:
 
         return response
 
-    def delete(self, path):
+    def delete(self, path: str) -> requests.Response:
         try:
             response = requests.delete(
                 self.base_api + path,
@@ -78,13 +81,13 @@ class BaseClient:
 
         return response
 
-    def _handle_response(self, response: requests.Response):
+    def _handle_response(self, response: requests.Response) -> dict:
         if not response.ok:
             raise self._relayer_exception(response)
 
         return response.json()
 
-    def _relayer_exception(self, response: requests.Response):
+    def _relayer_exception(self, response: requests.Response) -> RelayException:
         # TODO: @ssavarirayan add status code parsing and raise specific exceptions
         try:
             message = response.json()["message"]
@@ -110,32 +113,32 @@ class RelayClient(BaseClient):
         self.aws_srp_pool_region = aws_srp_pool_region
         self.base_api = "https://defender-api.openzeppelin.com/relayer/"
 
-    def get_relayer(self, relayer_id):
+    def get_relayer(self, relayer_id: str) -> requests.Response:
         response = self.get(f"relayers/{relayer_id}")
 
         return self._handle_response(response)
 
-    def list_relayers(self):
+    def list_relayers(self) -> requests.Response:
         response = self.get("relayers/summary")
 
         return self._handle_response(response)
 
-    def list_relayer_keys(self, relayer_id: str):
+    def list_relayer_keys(self, relayer_id: str) -> requests.Response:
         response = self.get(f"relayers/{relayer_id}/keys")
 
         return self._handle_response(response)
 
-    def create_relayer(self, data: dict) -> dict:
+    def create_relayer(self, data: dict) -> requests.Response:
         response = self.post("relayers", data)
 
         return self._handle_response(response)
 
-    def create_relayer_key(self, relayer_id: str) -> dict:
+    def create_relayer_key(self, relayer_id: str) -> requests.Response:
         response = self.post(f"relayers/{relayer_id}/keys")
 
         return self._handle_response(response)
 
-    def update_relayer(self, relayer_id: str, data: dict) -> dict:
+    def update_relayer(self, relayer_id: str, data: dict) -> requests.Response:
         initial_data = self.get_relayer(relayer_id)
 
         if "policies" in data:
@@ -149,14 +152,14 @@ class RelayClient(BaseClient):
 
         return self._handle_response(response)
 
-    def update_relayer_policies(self, relayer_id: str, data: dict):
+    def update_relayer_policies(self, relayer_id: str, data: dict) -> requests.Response:
         initial_data = self.get_relayer(relayer_id)["policies"]
         payload = initial_data | data
         response = self.put(f"relayers/{relayer_id}", payload)
 
         return self._handle_response(response)
 
-    def delete_relayer_key(self, relayer_id: str, key_id: str):
+    def delete_relayer_key(self, relayer_id: str, key_id: str) -> requests.Response:
         response = self.delete(f"relayers/{relayer_id}/keys/{key_id}")
 
         return self._handle_response(response)
@@ -177,27 +180,69 @@ class RelayerClient(BaseClient):
         self.aws_client_id = aws_client_id
         self.aws_srp_pool_region = aws_srp_pool_region
         self.base_api = "https://api.defender.openzeppelin.com/"
+        self.json_rpc_request_id = 0
 
-    def get_relayer(self):
-        pass
+    def get_relayer(self) -> requests.Response:
+        response = self.get("relayer")
 
-    def send_transaction(self):
-        pass
+        return self._handle_response(response)
 
-    def get_transaction_status(self):
-        pass
+    def send_transaction(self, data: dict) -> requests.Response:
+        response = self.post("txs", data)
 
-    def replace_transaction_by_id(self):
-        pass
+        return self._handle_response(response)
 
-    def replace_transaction_by_nonce(self):
-        pass
+    def get_transaction(self, tx_id: str) -> requests.Response:
+        response = self.get(f"txs/{tx_id}")
 
-    def list_transactions(self):
-        pass
+        return self._handle_response(response)
 
-    def sign_transaction(self):
-        pass
+    def replace_transaction_by_id(self, tx_id: str, data: dict) -> requests.Response:
+        response = self.put(f"txs/{tx_id}", data)
 
-    def call_json_rpc(self):
-        pass
+        return self._handle_response(response)
+
+    def replace_transaction_by_nonce(self, nonce: int, data: dict) -> requests.Response:
+        response = self.put(f"txs/{nonce}", data)
+
+        return self._handle_response(response)
+
+    def list_transactions(
+        self, status: str = None, limit: int = None, since: datetime.datetime = None
+    ) -> requests.Response:
+        params = {}
+
+        if status:
+            params["status"] = status
+        if limit:
+            params["limit"] = limit
+        if since:
+            params["since"] = since
+
+        response = self.get("txs", params)
+
+        return self._handle_response(response)
+
+    def sign(self, data: dict) -> requests.Response:
+        response = self.post("sign", data)
+
+        return self._handle_response(response)
+
+    def sign_typed_data(self, data: dict) -> requests.Response:
+        response = self.post("sign-typed-data", data)
+
+        return self._handle_response(response)
+
+    def call_json_rpc(self, method: str, params: typing.List[str]) -> requests.Response:
+        self.json_rpc_request_id += 1
+
+        data = {
+            "method": method,
+            "params": params,
+            "jsonrpc": "2.0",
+            "id": self.json_rpc_request_id,
+        }
+
+        response = self.post("relayer/jsonrpc", data)
+
+        return self._handle_response(response)
